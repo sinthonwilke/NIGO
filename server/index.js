05e7b7df92b75058886984fc84d81cf2bb4434f1
Sinthon Wilke <sinthonwilke@gmail.com>
2023-06-02 00:20:52 +0700

    index

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-// app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/', require('./routes/routes'));
