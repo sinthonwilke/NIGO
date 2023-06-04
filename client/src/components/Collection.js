fc9b16494d792246dafdf6e86e1b9da59bec2394
Sinthon Wilke <sinthonwilke@gmail.com>
2023-06-05 02:59:04 +0700

    save

---
+++ client/src/components/Collection.js
@@ -0,0 +1,11 @@
+
+
+function Collection() {
+    return (
+        <div>
+            <h1>Collection</h1>
+        </div>
+    );
+}
+
+export default Collection;
--- client/src/pages/WishListPage.js
+++ client/src/pages/WishListPage.js
@@ -1,11 +1,13 @@
 import gStyles from '../styles/global.module.css';
 import CreateCollection from '../components/CreateCollection';
+import Collection from '../components/Collection';
 
 function WishListPage() {
     return (
         <>
             <h1 className={gStyles.head}>WishList</h1>
             <CreateCollection />
+            <Collection />
         </>
     );
 }
