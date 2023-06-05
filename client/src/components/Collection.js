64496895382e451afad81acd583df59e2eb78cf2
Sinthon Wilke <sinthonwilke@gmail.com>
2023-06-05 14:04:42 +0700

    style

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,32 +1,47 @@
+import { useState } from 'react';
 import styles from '../styles/Collection.module.css';
 import { BiRename } from 'react-icons/bi';
 import { AiFillDelete } from 'react-icons/ai';
 import example from '../assets/example.jpg';
+import { FiMoreHorizontal, FiMoreVertical } from 'react-icons/fi';
+import { CgMoreO, CgMoreVerticalO } from 'react-icons/cg';
+
+
 function Collection() {
+    const [showMore, setShowMore] = useState(true);
+
+    const handleViewMore = () => {
+        setShowMore(!showMore);
+    };
+
     return (
         <>
             <div className={styles.collection}>
                 <div className={styles.head}>
                     <h3>Test</h3>
                     <div className={styles.btnGroup}>
+                        <button className={styles.switchBtn} onClick={handleViewMore}>
+                            {showMore ? <CgMoreO className={styles.icon} /> : <CgMoreVerticalO className={styles.icon} />}
+                        </button>
                         <button className={styles.btn}><BiRename className={styles.icon} /></button>
                         <button className={styles.btn}><AiFillDelete className={styles.icon} /></button>
                     </div>
                 </div>
                 <div className={styles.line} />
-                <div className={styles.body}>
-                    <div className={styles.item}>
-                        <img src={example} alt="example" />
-                        <img src={example} alt="example" />
-                        <img src={example} alt="example" />
-                        <img src={example} alt="example" />
-                        <img src={example} alt="example" />
+                <div className={`${styles.body} ${showMore ? styles.showMore : ''}`}>
+                    <div className={showMore ? styles.item : styles.showmore}>
+                        <>
+                            <img src={example} alt="example" />
+                            <img src={example} alt="example" />
+                            <img src={example} alt="example" />
+                            <img src={example} alt="example" />
+                            <img src={example} alt="example" />
+                        </>
                     </div>
                 </div>
-                <button className={styles.switchBtn}>View More</button>
+
             </div>
         </>
-
     );
 }
 
