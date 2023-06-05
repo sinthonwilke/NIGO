dfe7873398cc74769f2bf8e2962e88bb65d82388
Sinthon Wilke <sinthonwilke@gmail.com>
2023-06-05 15:04:48 +0700

    style

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,30 +1,60 @@
 import { useState } from 'react';
 import styles from '../styles/Collection.module.css';
 import { BiRename } from 'react-icons/bi';
-import { AiFillDelete } from 'react-icons/ai';
 import example from '../assets/example.jpg';
-import { FiMoreHorizontal, FiMoreVertical } from 'react-icons/fi';
 import { CgMoreO, CgMoreVerticalO } from 'react-icons/cg';
+import { TbTrashXFilled } from 'react-icons/tb';
 
 
 function Collection() {
+    const [textValue, setTextValue] = useState('Test');
     const [showMore, setShowMore] = useState(true);
+    const [isRenaming, setIsRenaming] = useState(false);
+    const [updatedTextValue, setUpdatedTextValue] = useState(textValue);
+
 
     const handleViewMore = () => {
         setShowMore(!showMore);
+    };
+
+    const handleRename = () => {
+        setUpdatedTextValue(textValue);
+        setIsRenaming(!isRenaming);
+    };
+
+    const handleTextChange = (event) => {
+        setUpdatedTextValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setTextValue(updatedTextValue);
+        setIsRenaming(false);
     };
 
     return (
         <>
             <div className={styles.collection}>
                 <div className={styles.head}>
-                    <h3>Test</h3>
+                    {isRenaming ? (
+                        <form onSubmit={handleSubmit} className={styles.form}>
+                            <input
+                                type="text"
+                                value={updatedTextValue}
+                                onChange={handleTextChange}
+                                autoFocus
+                                className={styles.headerText}
+                            />
+                        </form>
+                    ) : (
+                        <p className={styles.headerText}>{textValue}</p>
+                    )}
                     <div className={styles.btnGroup}>
                         <button className={styles.switchBtn} onClick={handleViewMore}>
                             {showMore ? <CgMoreO className={styles.icon} /> : <CgMoreVerticalO className={styles.icon} />}
                         </button>
-                        <button className={styles.btn}><BiRename className={styles.icon} /></button>
-                        <button className={styles.btn}><AiFillDelete className={styles.icon} /></button>
+                        <button className={styles.btn} onClick={handleRename}><BiRename className={styles.icon} /></button>
+                        <button className={styles.btn}><TbTrashXFilled className={styles.icon} /></button>
                     </div>
                 </div>
                 <div className={styles.line} />
